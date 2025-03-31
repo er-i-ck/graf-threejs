49a206622db7061bd15addffba9334a93f873b8f
er-i-ck <erickalejandrort@gmail.com>
2025-03-30 19:36:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.154.0/build/three.module.js';
+import * as THREE from 'three'; 
 
 // Crear la escena, cámara y renderizador
 const scene = new THREE.Scene();
